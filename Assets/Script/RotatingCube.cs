79afe460bc44006bf811dd76d3e2da3e0a738d2b
ArashAnbari <artash.art@gmail.com>
2022-12-10 11:17:42 +0100

    W Rotate
    rotate with keyboard W

--- Assets/Script/RotatingCube.cs
+++ Assets/Script/RotatingCube.cs
@@ -1,23 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class RotatingCube : MonoBehaviour
 {
     public Vector3 editPosition;
     public Vector3 editRotation;
     public Vector3 editScale;
+    public Keyboard keyboard;
+
 
     // Start is called before the first frame update
     void Start()
     {
-      
+        keyboard = Keyboard.current;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(editRotation);
-        Debug.Log(transform.position);
+
+        if (keyboard == null)
+        {
+            Debug.Log("No keyboard");
+            return;
+        }
+
+        if (keyboard.wKey.isPressed)
+        {
+            transform.Rotate(editRotation * Time.deltaTime);
+            Debug.Log("Update");
+        }
     }
 }
