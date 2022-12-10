b6a193f83fb78f9191b9fedd920d8177e27391cd
ArashAnbari <artash.art@gmail.com>
2022-12-10 12:26:08 +0100

    WASD keyboard
    now "WASD" works as it should!
    Welldone!

--- Assets/Script/RotatingCube.cs
+++ Assets/Script/RotatingCube.cs
@@ -5,16 +5,18 @@ using UnityEngine.InputSystem;
 
 public class RotatingCube : MonoBehaviour
 {
-    public Vector3 editPosition;
-    public Vector3 editRotation;
-    public Vector3 editScale;
+    //public Vector3 editPosition;
+    //public Vector3 editRotation;
+    //public Vector3 editScale;
     public Keyboard keyboard;
+   
 
 
     // Start is called before the first frame update
     void Start()
     {
         keyboard = Keyboard.current;
+        
     }
 
     // Update is called once per frame
@@ -29,8 +31,34 @@ public class RotatingCube : MonoBehaviour
 
         if (keyboard.wKey.isPressed)
         {
-            transform.Rotate(editRotation * Time.deltaTime);
+            transform.position = transform.position + new Vector3(0, 0, 0.01f);
+            Debug.Log("Update");
+            //transform.Rotate(editRotation * Time.deltaTime);
+            //Debug.Log("Update");
+        }
+
+        if (keyboard.sKey.isPressed)
+        {
+            transform.position = transform.position + new Vector3(0, 0, -0.01f);
+            Debug.Log("Update");
+            //transform.Rotate(editRotation * Time.deltaTime);
+            //Debug.Log("Update");
+        }
+
+        if (keyboard.aKey.isPressed)
+        {
+            transform.position = transform.position + new Vector3(0.01f, 0, 0);
             Debug.Log("Update");
         }
+
+
+        if (keyboard.dKey.isPressed)
+        {
+            transform.position = transform.position + new Vector3(-0.01f, 0, 0);
+            Debug.Log("Update");
+        }
+
+        //transform.position = transform.position + new Vector3(keyboard.wKey * 5 * Time.deltaTime, verticalInput * 5 * Time.deltaTime, 0);
     }
+
 }
